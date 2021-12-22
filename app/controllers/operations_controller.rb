a12e5b4de254ca6f0928055d2a98ed467b0e1c85
exit <bentkhayatyessine79@gmail.com>
2021-12-22 12:53:52 +0100

    added ip verif

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -7,9 +7,24 @@ class OperationsController < ApplicationController
 		#require "shodanz"
 		api = Shodanz.client.new(key: "oEghZYqiofqOQqKlRhlmMnhPfFjrmpza")
 
-		results = api.host("#{params[:IP]}")
-		blingbling = results['data']
-		#render plain: "#{params[:IP]}"
-		render plain: "#{blingbling}"
+		ip = params[:IP]
+		if verif_ip(ip)
+			@host = api.host(ip)
+			#blingbling = host['data']
+			#render plain: "#{params[:IP]}"
+			render :search
+		else
+			render :new, notice: "enter a valid ip address!"
+		end
+	end
+
+	private def verif_ip(str)
+		require 'resolv'	 	 
+		case str
+		when Resolv::IPv4::Regex
+			return true
+		else
+			return false
+		end
 	end
 end
