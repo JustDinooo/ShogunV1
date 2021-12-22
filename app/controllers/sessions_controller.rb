2a468bfb36cc284a6a9861963bc17c4f3ac8a8d2
exit <bentkhayatyessine79@gmail.com>
2021-12-22 18:37:46 +0100

    modified redirection

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email])
 		if user.present? && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect_to root_path, notice: "Signed in successfully!"
+			redirect_to shogun_path, notice: "Signed in successfully!"
 		else
 			flash[:alert] = "Invalid email or password!"
 			render :new
